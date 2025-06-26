996ddb3771fe53e7fe253223268e43622cd8af95
linkbilalali <link.bilalali@gmail.com>
2025-06-26 15:39:00 +0100

    Optimized feature detection

--- *.cpp
+++ *.cpp
@@ -366,3 +366,4 @@
 # OpenCV System Log: 2025-06-19 - Update 69
 # OpenCV System Log: 2025-06-24 - Update 77
 # OpenCV System Log: 2025-06-26 - Update 81
+# OpenCV System Log: 2025-06-26 - Update 82
