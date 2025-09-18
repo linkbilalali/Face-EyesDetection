762a76c051efcb4f89d347bfab6bbb8f9164e14f
linkbilalali <link.bilalali@gmail.com>
2025-09-18 14:23:00 +0100

    Improved frame rate

--- *.cpp
+++ *.cpp
@@ -394,3 +394,4 @@
 # OpenCV System Log: 2025-09-12 - Update 243
 # OpenCV System Log: 2025-09-14 - Update 247
 # OpenCV System Log: 2025-09-15 - Update 248
+# OpenCV System Log: 2025-09-18 - Update 253
