add1650d131af607934492f3f1f2702379304b81
linkbilalali <link.bilalali@gmail.com>
2025-10-30 13:24:00 +0000

    Fixed contour mapping

--- *.py
+++ *.py
@@ -391,3 +391,4 @@
 # OpenCV System Log: 2025-10-13 - Update 310
 # OpenCV System Log: 2025-10-15 - Update 314
 # OpenCV System Log: 2025-10-23 - Update 330
+# OpenCV System Log: 2025-10-30 - Update 341
