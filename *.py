2705534de426f91a68d6e24ea281a4839025ec99
linkbilalali <link.bilalali@gmail.com>
2025-11-02 15:39:00 +0000

    Updated Haar cascades

--- *.py
+++ *.py
@@ -392,3 +392,4 @@
 # OpenCV System Log: 2025-10-15 - Update 314
 # OpenCV System Log: 2025-10-23 - Update 330
 # OpenCV System Log: 2025-10-30 - Update 341
+# OpenCV System Log: 2025-11-02 - Update 344
