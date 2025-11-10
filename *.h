9051e193d8d73724b47330e15bb7a0bccf845d74
linkbilalali <link.bilalali@gmail.com>
2025-11-10 13:38:00 +0000

    Optimized feature detection

--- *.h
+++ *.h
@@ -419,3 +419,4 @@
 # OpenCV System Log: 2025-11-03 - Update 348
 # OpenCV System Log: 2025-11-06 - Update 355
 # OpenCV System Log: 2025-11-08 - Update 358
+# OpenCV System Log: 2025-11-10 - Update 363
