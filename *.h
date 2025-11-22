162868e351974864cc48bbbc1fd97b3056de4585
linkbilalali <link.bilalali@gmail.com>
2025-11-22 11:55:00 +0000

    Tuned blur filters

--- *.h
+++ *.h
@@ -422,3 +422,4 @@
 # OpenCV System Log: 2025-11-10 - Update 363
 # OpenCV System Log: 2025-11-11 - Update 366
 # OpenCV System Log: 2025-11-19 - Update 381
+# OpenCV System Log: 2025-11-22 - Update 386
